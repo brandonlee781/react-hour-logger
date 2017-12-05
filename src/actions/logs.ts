669a47ffebefe669cd187a134101497082658e33
Brandon Lee <brandonlee781@gmail.com>
2017-12-05 08:00:11 -0600

    More thunk -> apollo changes

--- src/actions/logs.ts
+++ src/actions/logs.ts
@@ -1,17 +1,41 @@
 import { Dispatch } from 'redux';
-import axios, { AxiosResponse } from 'axios';
+// import axios, { AxiosResponse } from 'axios';
 import { Log } from '../constants/types';
+// import { client } from '../store/client';
+// import { ApolloQueryResult } from 'apollo-client';
+// import gql from 'graphql-tag';
 export const REQUEST_LOGS = 'REQUEST_LOGS';
 export const RECEIVE_LOGS = 'RECEIVE_LOGS';
 export const RECEIVE_LOGS_FAILED = 'RECEIVE_LOGS_FAILED';
 
-axios.defaults.adapter = require('axios/lib/adapters/http');
-const api = axios.create({
-  baseURL: 'https://www.branlee.me/api/v1/logs',
-  headers: {
-    'Authorization': 'Bearer 935dce7e662f66a454041422ccfab4289deebf4c',
-  },
-});
+// axios.defaults.adapter = require('axios/lib/adapters/http');
+// const api = axios.create({
+//   baseURL: 'https://www.branlee.me/api/v1/logs',
+//   headers: {
+//     'Authorization': 'Bearer 935dce7e662f66a454041422ccfab4289deebf4c',
+//   },
+// });
+
+// const authorization = 
+//   'bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ0ZXN0In0.ixcKwo-4Ate8Gb88ybx4IBCric40yKSXjDAEePn3IlY';
+// const query = gql`
+//   {
+//     allLogs{
+//       logs{
+//         id,
+//         date,
+//         startTime,
+//         endTime,
+//         duration,
+//         project{
+//           id,
+//           name
+//         },
+//         note
+//       }
+//     }
+//   }
+// `;
 
 export function requestLogs() {
   return {
@@ -32,12 +56,20 @@ export function recieveLogsFailed() {
   };
 }
 
+// interface LogResponse {
+//   allLogs: {
+//     logs: Log[]
+//   };
+// }
+
 export function fetchLogs() {
   return async function(dispatch: Dispatch<{}>) {
     try {
-      dispatch(requestLogs());
-      const res: AxiosResponse = await api.get('/'); 
-      dispatch(receiveLogs(res.data));
+      // dispatch(requestLogs());
+      // const res: ApolloQueryResult<LogResponse> = await client.query<LogResponse>({
+      //   query,
+      // });
+      // dispatch(receiveLogs(res.data.allLogs.logs));
     } catch (err) {
       console.error(err);
       dispatch(recieveLogsFailed());
