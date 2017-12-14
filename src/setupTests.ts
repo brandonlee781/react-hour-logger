ad89f6ef11344bbb22c72ac694907b28e81a36dd
Brandon Lee <brandonlee781@gmail.com>
2017-12-13 21:51:03 -0600

    Fix raf error message in tests

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,12 +1,12 @@
+// tslint:disable-next-line:no-any
+(global as any).requestAnimationFrame = function(callback: any) {
+  setTimeout(callback, 0);
+};
+
 import { Mount } from './constants/types';
 import { configure } from 'enzyme';
 import * as Adapter from 'enzyme-adapter-react-16';
 import { jsdom } from 'jsdom';
-import 'raf/polyfill';
+// import 'raf/polyfill';
 
 configure({ adapter: new Adapter() });
-
-global.requestAnimationFrame = function(callback: {}) {
-  setTimeout(callback, 0);
-  return 0;
-};
